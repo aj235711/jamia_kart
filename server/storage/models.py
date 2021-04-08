730477ddd2387fb94b8087c0bcb341cd8ce5380e
TanmayVig <tanmayvij11@gmail.com>
2021-04-08 14:52:15 +0530

    user creation message changed

--- server/storage/models.py
+++ server/storage/models.py
@@ -3,9 +3,15 @@ from sqlalchemy.orm import relationship
 
 from storage.database import Base
 
+# class User(Base):
+#     __tablename__="user"
+#     email=Column(String(length=100), primary_key=True,unique=True, )
+#     name=Column(String(length=100))
+#     password=Column(String(length=10))
+#     category=Column(String(length=10))
 class User(Base):
     __tablename__="user"
-    email=Column(String(length=100), primary_key=True,unique=True, )
-    name=Column(String(length=100))
-    password=Column(String(length=10))
-    category=Column(String(length=10))
+    email=Column(String, primary_key=True,unique=True, )
+    name=Column(String)
+    password=Column(String)
+    category=Column(String)
