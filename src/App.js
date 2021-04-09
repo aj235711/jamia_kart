27f64f5113376798274ed2e930773617a2a05d04
Aman malik <amanmaliknhr@gmail.com>
2021-04-09 19:11:58 +0530

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-
+import axios from "axios";
+import HomePage from "./Components/HomePage"
 import LoginForm from './Components/LoginForm';
 import RegisterForm from './Components/RegisterForm';
 
 
 const App = () => {
+  axios.get('https://fakestoreapi.com/products/1').then(res => console.log(res));
   return (
     <>
-      <RegisterForm /><LoginForm />
+      {/* <RegisterForm /><LoginForm /> */}
+      <HomePage />
       </>
   );
 };
