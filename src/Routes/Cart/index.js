b88eb647566303610aaeb5afbee6b1a9273a92db
aj235711 <amirjaved68504@gmail.com>
2021-04-27 01:56:31 +0530

    add cart route

--- src/Routes/Cart/index.js
+++ src/Routes/Cart/index.js
@@ -3,15 +3,16 @@ import {Col} from 'reactstrap';
 
 import NavBar from "../../Components/NavBar.js";
 import Nav2 from "../../Components/Nav2.js";
+import CartItem from './Components/CartItems';
 
 const Cart = () => {
   return (
     <>
       <NavBar />
       <Nav2 />
-      <Col md="12" className="bg-light border border-secondary" style={{height: '30vh'}}>
-          <Col md="3" className="h-100 d-flex align-items-center justify-content-center" style={{borderRight: '2px solid #444'}}>
-              image yahan aayegi
+      <Col md="12" className="d-flex justify-content-center mt-5">
+        <Col md="9" className="d-flex align-items-center flex-wrap">
+          <CartItem id={0}/>
           </Col>
       </Col>
     </>
