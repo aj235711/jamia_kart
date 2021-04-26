a5fa1f71c17219bf88191a1b96c25fc7f040e319
TanmayVig <tanmayvij11@gmail.com>
2021-04-26 18:03:33 +0530

    order create route completed

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ from starlette.middleware.cors import CORSMiddleware
 import storage.schema as schema
 import storage.database as database
 import storage.models as models
-from routes import  user, authentication,admin,seller,costumer,product,cart
+from routes import  user, authentication,admin,seller,costumer,product,cart, order
 from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -18,6 +18,7 @@ app.include_router(seller.route)
 app.include_router(costumer.route)
 app.include_router(product.route)
 app.include_router(cart.route)
+app.include_router(order.route)
 
 origins = [
     "http://localhost:3000",
---
+++ server/routes/order.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, HTTPException, status, Response
+from sqlalchemy.orm import Session
+from typing import List,Optional
+from storage import schema,database,models
+from security import oauth2
+
+route=APIRouter(
+    prefix="/order",
+    tags=["Order"]
+)
+
+get_current_user=oauth2.get_current_user
+get_db = database.get_db
+
+@route.post("/")
+def create_order(request : schema.Order, db : Session = Depends(get_db), current_user : schema.User=Depends(get_current_user)):
+    user = db.query(models.User).filter(models.User.email==current_user.email).first()
+    if not user or user.category!="customer":
+        return {"success":False, "errMsg": "not a customer"}
+    product = db.query(models.Product).filter(models.Product.id==request.product_id)
+    if not product.first() or product.first().qty<request.qty:
+        return {"success":False,"errMsg":"product not available try decreasing quantity"}
+    amount = product.first().price*request.qty
+    shipp_add = request.shipping_add
+    prod_id = request.product_id
+    qnty = request.qty
+    if shipp_add == "":
+        shipp_add = db.query(models.Costumer).filter(models.Costumer.id==user.costumer_id).first().loc
+    if request.cart_id != -1:
+        cart = db.query(models.Cart).filter(models.Cart.id==request.cart_id)
+        if not cart.first() or cart.first().product_id != request.product_id or cart.first().qty != request.qty:
+            return {"success":False,"errMsg":"invalid id"}
+        prod_id = cart.first().product_id
+        qnty = cart.first().qty
+        cart.delete()
+    order = models.Order(qty=qnty,costumer_id=user.costumer_id,product_id=prod_id,ship_add=shipp_add,amount=amount)
+    db.add(order)
+    product.update({"qty":product.first().qty-request.qty},synchronize_session=False)
+    db.commit()
+    db.refresh(order)
+    return {"success":True, "msg":"order created","order_id":order.id}
+
